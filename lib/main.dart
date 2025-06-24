372208e57b124c66733653f913ac038626bca451
Ahmad Abdulkarim <123482290+Ahmaddoolt@users.noreply.github.com>
2025-06-24 13:47:39 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
-// lib/main.dart
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:unicurve/core/localization/translations.dart';
-import 'package:unicurve/pages/auth/login/login_page.dart';
-import 'package:unicurve/pages/uni_admin/providers/settings_provider.dart';
-
-// 1. IMPORT YOUR CUSTOM THEME FILE
-import 'package:unicurve/core/theme/app_theme.dart'; // Make sure this path is correct
+import 'package:unicurve/core/theme/app_theme.dart';
+import 'package:unicurve/pages/initialization_screen.dart'; // Import the new screen
+import 'package:unicurve/settings/settings_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://yourUrl.supabase.co',
+    url: 'https://llsiaszwjqejufchefds.supabase.co',
     anonKey:
-        'yourKey',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxsc2lhc3p3anFlanVmY2hlZmRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4NjQwOTcsImV4cCI6MjA2NDQ0MDA5N30.RYxhKWgu7tvohsRVqmh_rt1UFEJTojsG2O2WlRQJ6S4',
   );
 
   runApp(const ProviderScope(child: MyApp()));
@@ -34,20 +30,13 @@ class MyApp extends ConsumerWidget {
     return GetMaterialApp(
       title: 'UniCurve',
       debugShowCheckedModeBanner: false,
-
-      // --- Localization Settings ---
       translations: AppTranslations(),
       locale: locale,
       fallbackLocale: const Locale('en', 'US'),
-      
-      // --- THEME SETTINGS (CORRECTED) ---
       themeMode: themeMode,
-
-      // 2. APPLY YOUR CUSTOM THEMES HERE
-      theme: AppTheme.lightTheme,      // USE YOUR CUSTOM LIGHT THEME
-      darkTheme: AppTheme.darkTheme,  // USE YOUR CUSTOM DARK THEME
-
-      home: const LoginPage(),
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      home: const InitializationScreen(),
     );
   }
 }
