1fbbd0870cddfdd6c1211e8017a6628ab17bfe4c
Ahmad Abdulkarim <123482290+Ahmaddoolt@users.noreply.github.com>
2025-06-24 12:00:03 +0300

    Delete auth directory

--- auth/signup/student_signup/student_signup_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:unicurve/pages/auth/signup/student_signup/student_signup_controller.dart';
-
-class SignupBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<SignupController>(() => SignupController());
-  }
-}
